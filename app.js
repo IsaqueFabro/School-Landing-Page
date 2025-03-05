34ad660e991634229ea3f37730026c6d2c42e821
Isaque Fabro <isaquefabro04@gmail.com>
2025-03-04 21:54:18 -0300

    projeto finalizado, comentarios restantes adicionados, e formatados para o mesmo padrao, testes finais realizados!

--- app.js
+++ app.js
@@ -1,19 +1,22 @@
+// variaveis para os elementos das setas e cards
 var setaEsquerda = window.document.getElementById("seta-esquerda");
 var setaDireita = window.document.getElementById("seta-direita");
 var leonardo = window.document.getElementById('leonardo');
 var samantha = window.document.getElementById('samantha');
 var bruna = window.document.getElementById('bruna');
 
+// funcao para rolar para a direita (mostrar bruna e esconder leonardo)
 function RolarParaDireita() {
-    leonardo.style ="display: none";
-    bruna.style ="display: flex";
-    setaDireita.style ="display: none"
-    setaEsquerda.style ="display: flex; margin-top: 50%"
+    leonardo.style ="display: none"; // esconde leonardo
+    bruna.style ="display: flex"; // mostra bruna
+    setaDireita.style ="display: none"; // esconde seta direita
+    setaEsquerda.style ="display: flex; margin-top: 50%"; // mostra seta esquerda
 }
 
-function RolarParaEsquerda() {
-    bruna.style ="display: none";
-    leonardo.style ="display: flex";
-    setaDireita.style ="display: flex; margin-top: 50%"
-    setaEsquerda.style ="display: none"
+// funcao para rolar para a esquerda (mostrar leonardo e esconder bruna)
+function RolarParaEsquerda(){
+    bruna.style ="display: none"; // esconde bruna
+    leonardo.style ="display: flex"; // mostra leonardo
+    setaDireita.style ="display: flex; margin-top: 50%"; // mostra seta direita
+    setaEsquerda.style ="display: none"; // esconde seta esquerda
 }
